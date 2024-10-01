18d2560a3246d2099b7aa9f576a71ac194b8ac88
Satoji <48027468+Satoji@users.noreply.github.com>
2024-09-30 20:36:33 -0700

    removed test.js file

--- test.js
+++
@@ -1,5 +0,0 @@
-const expression = "3 * 4 * 2";
-const [left, right] = expression.split('*').map(Number);
-const result = left * right;
-console.log(result); // Outputs: 12
-console.log("e: " + 5.56e+14*1)
